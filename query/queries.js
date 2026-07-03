bc95b5cf5308ba821e19d548cd6db0a5b383ae53
CSC <82180341+cscmx@users.noreply.github.com>
2026-07-03 15:43:21 +0200

    Update queries.js
    querie 5

--- query/queries.js
+++ query/queries.js
@@ -11,7 +11,7 @@ db.restaurants.find({},{_id: 0, restaurant_id:1, name:1, borough:1, cuisine: 1})
 db.restaurants.find({},{_id: 0, restaurant_id:1, name:1, borough:1, "address.zipcode":1})
 
 // 5. Mostrar tots els restaurants que estan en el Bronx.
-
+db.restaurants.find({},{_id: 0, borough: 'Bronx'})
 
 // 6. Mostrar els primers 5 restaurants que estan en el Bronx.
 
